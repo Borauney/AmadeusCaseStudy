3e1c42db9aeae252f67d103ad36bb921b2e7d4c7
Borauney <74118069+Borauney@users.noreply.github.com>
2023-09-02 14:23:40 +0300

    Delete caseStudy/caseStudy directory

--- caseStudy/caseStudy/src/main/java/caseStudy/caseStudy/repository/HavaalaniRepository.java
+++
@@ -1,9 +0,0 @@
-package caseStudy.caseStudy.repository;
-
-import caseStudy.caseStudy.entity.Havaalani;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HavaalaniRepository extends JpaRepository<Havaalani,Long> {
-
-    Havaalani getHavaAlaniBySehir(String sehir);
-}
